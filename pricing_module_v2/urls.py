f7c9e4174bac654f6b640bed9800ae3471625952
Saurabh8527 <singh.saurabh8527@gmail.com>
2023-08-02 23:56:48 +0530

    api added for calculating price

--- pricing_module_v2/urls.py
+++ pricing_module_v2/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('pricing_config/', include('pricing_config.urls')),
 ]
